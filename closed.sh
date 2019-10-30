98f4c7ea016eb71523159dd751056bb12f69e900
floriansegginger <florian@fullrange.ch>
2019-10-30 17:47:31 +0100

    Remove loop in closed.sh

--- closed.sh
+++ closed.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-while [[ 1 ]]; do
-  ffmpeg -re -stream_loop -1 -fflags +genpts -re -i closed.flv -r 15 -f flv rtmp://a.rtmp.youtube.com/live2/840v-xz8s-bvuc-17fb
-done
+ffmpeg -re -stream_loop -1 -fflags +genpts -re -i closed.flv -r 15 -f flv rtmp://a.rtmp.youtube.com/live2/840v-xz8s-bvuc-17fb
