9f819c354f4aec4994a1c219f119d0bd889668dc
floriansegginger <florian@fullrange.ch>
2019-11-06 14:54:25 +0000

    Update

--- closed.sh
+++ closed.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -re -stream_loop -1 -fflags +genpts -re -i closed.flv -r 15 -f flv rtmp://a.rtmp.youtube.com/live2/840v-xz8s-bvuc-17fb
+ffmpeg -re -stream_loop -1 -re -i closed-15fps.flv -f flv rtmp://x.rtmp.youtube.com/live2/tgew-9huf-w4r1-0jf9
